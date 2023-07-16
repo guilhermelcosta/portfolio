9c74d224f5fa3fa69cbc6a65dfc0487127b9b1be
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-07-15 21:00:51 -0300

    feat: finalizado media queries para mobile

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,19 +5,35 @@ import AboutMe from '../sections/AboutMe';
 import Skills from '../sections/Skills';
 import Languages from '../sections/Languages';
 import Education from '../sections/Education';
+import {useEffect, useState} from 'react';
 
 export default function Home() {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <section className={styles.grid_container}>
             <div className={styles.column}>
                 <Heading/>
+                {isMobile && <ContactInfo/>}
                 <AboutMe/>
                 <Skills/>
             </div>
             <div className={styles.column}>
                 <Languages/>
                 <Education/>
-                <ContactInfo/>
+                {!isMobile && <ContactInfo/>}
             </div>
         </section>
     );
