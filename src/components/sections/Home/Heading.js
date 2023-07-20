34dc8a55f56f21529b06dfd0806b3617fced8c26
Guilherme Lage da costa <guilhermeldcosta@gmail.com>
2023-07-20 10:19:48 -0300

    feat: atualizado foto e AboutMe.js

--- src/components/sections/Home/Heading.js
+++ src/components/sections/Home/Heading.js
@@ -1,10 +1,10 @@
 import styles from './Heading.module.css';
-import img_placeholder from '../../../img/foto-placeholder.jpg';
+import foto from '../../../img/foto-heading.jpeg';
 
 export default function Heading() {
     return (
         <div className={styles.heading}>
-            <img src={img_placeholder} alt="Minha foto"/>
+            <img src={foto} alt="Minha foto"/>
             <div>
                 <h1>Olás, eu sou o Guilherme <span>👋</span></h1>
                 <h2>Desenvolvedor full stack</h2>
